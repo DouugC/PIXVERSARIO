75af83dc7a69e670f36705e72756ee06bc6d7747
Douglas Costa <69803913+DouugC@users.noreply.github.com>
2025-11-25 11:12:19 -0300

    Update input prompts for number range selection
    Ajustando o texto que o usuário irá ler

--- PIXVERSARIO.py
+++ PIXVERSARIO.py
@@ -1,8 +1,8 @@
 import random
 
 print("PIXVERSARIO, escolha um intervalo de dois numeros e sorteie seu pix ! ")
-PrimeiroNumero = input("Digite o primeiro numero : ")
-UltimoNumero = input("Digite o Ultimo numero : ")
+PrimeiroNumero = input("Digite o valor mínimo (apenas números inteiros) : ")
+UltimoNumero = input("Digite o valor máximo (apenas números inteiros) : ")
 
 PrimeiroNumero = (int(PrimeiroNumero))
 UltimoNumero = (int(UltimoNumero))
